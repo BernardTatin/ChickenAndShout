7f0a15b88ebcc3e05c1a66e9d086b2c51afa2f1e
BernardTatin <bernard.tatin@outlook.fr>
2016-12-10 00:19:00 +0100

    Factorielles pour owl-lisp

--- fun/continuations/fact.scm
+++ fun/continuations/fact.scm
@@ -1,21 +1,30 @@
 ;; ======================================================================
 ;; fact.scm
 ;; une factorielle avec continuation
+;; owl-lisp:
+;; ol -o fact.c fact.scm&& gcc -o fact fact.c && ./fact 5
 ;; ======================================================================
 
-(define k-fact (lambda(N k)
-				 (define ik-fact (lambda(i acc)
-								   (if (< i 1)
-									 (k acc)
-									 (ik-fact (- i 1) (* acc i)))))
-				 (ik-fact N 1)))
+(define newline
+  (lambda()
+	(print "")))
 
-(define show-facts (lambda(N)
-					 (define iloop (lambda(i)
-									 (when (<= i N)
-									   (k-fact i (lambda(e) (display e) (newline)))
-									   (iloop (+ i 1)))))
-					 (iloop 1)))
+(define k-fact 
+  (lambda(value0 N k op)
+	(define ik-fact (lambda(i acc)
+					  (if (< i 1)
+						(k acc)
+						(ik-fact (- i 1) (op acc i)))))
+	(ik-fact N value0)))
 
-(show-facts 10)
+(define show-facts 
+  (lambda(value0 N op)
+	(define iloop (lambda(i)
+					(when (<= i N)
+					  (k-fact value0 i (lambda(e) (display i) (display " -> ") (display e) (newline)) op)
+					  (iloop (+ i 1)))))
+	(iloop 1)))
+
+(lambda(args)
+  (show-facts 1 (string->number (car (cdr args))) *))
 
