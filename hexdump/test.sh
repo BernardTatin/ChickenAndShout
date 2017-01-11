be05d91d7a7cae6a1366bf660fafe64a223585eb
Bernard Tatin <bernard.tatin@outlook.fr>
2017-01-11 09:22:58 +0100

    A better test script

--- hexdump/test.sh
+++ hexdump/test.sh
@@ -17,26 +17,46 @@ rm -fv appinone-hexdump.scm hexdump.exe
 echo 'init ========================================'
 [[ -f tfile ]] || (cat **/*.scm > tfile; rm ref.log)
 [[ -f ref.log ]] || hexdump -C -v tfile | tr -s ' ' > ref.log
+export issagittarius=$(which sagittarius)
+export isgosh=$(which gosh)
+export isfoment=$(which foment)
+export ischicken=$(which csc)
 
-echo 'sagittarius ========================================'
-time sagittarius -r 7 -L . ./hexdump.scm tfile | tr -s ' ' > sagittarius.log
+if [[ -n $issagittarius ]]
+then
+    echo 'sagittarius ========================================'
+    time sagittarius -r 7 -L . ./hexdump.scm tfile | tr -s ' ' > sagittarius.log
+fi
 
-echo 'gosh ========================================'
-time gosh -r 7 -I ../lib -I . ./hexdump.scm tfile | tr -s ' ' > gosh.log
+if [[ -n $isgosh ]]
+then
+    echo 'gosh ========================================'
+    time gosh -r 7 -I ../lib -I . ./hexdump.scm tfile | tr -s ' ' > gosh.log
+fi
 
-echo 'foment ========================================'
-time foment -I ../lib -I . ./hexdump.scm tfile | tr -s ' ' > foment.log
+if [[ -n $isfoment ]]
+then
+    echo 'foment ========================================'
+    time foment -I ../lib -I . ./hexdump.scm tfile | tr -s ' ' > foment.log
+fi
 
-echo 'chicken ========================================'
-${MAKE} all &> mak.log
-time ./hexdump.exe tfile | tr -s ' ' > chicken.log
+if [[ -n $ischicken ]]
+then
+    echo 'chicken ========================================'
+    ${MAKE} all &> mak.log
+    time ./hexdump.exe tfile | tr -s ' ' > chicken.log
+fi
 
 echo 'done ========================================'
 
 
 for sch in sagittarius gosh foment chicken
 do
-	echo "======================================================================"
-	echo "$sch"
-	diff -EbBw ref.log $sch.log > /dev/null || echo "FAILED !!!!"
+    test=is${sch}
+    if [[ -n ${!test} ]]
+    then
+    	echo "======================================================================"
+    	echo "$sch"
+    	diff -EbBw ref.log $sch.log > /dev/null || echo "FAILED !!!!"
+    fi
 done
