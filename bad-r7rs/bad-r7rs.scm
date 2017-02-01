68a001573876d3318c4e5853cc05a390a7fc66d7
BernardTatin <bernard.tatin@outlook.fr>
2017-02-02 00:17:01 +0100

    A better lint - still stupid

--- bad-r7rs/bad-r7rs.scm
+++ bad-r7rs/bad-r7rs.scm
@@ -3,11 +3,11 @@
 ;; Read r7rs sources files and interpret them
 ;; written with r5rs and some extensions
 ;; for guile 2.0.xx
-;;	guile		: OK
-;;  gosh		: FAIL      (gosh -r 7 bad-r7rs.scm, can't find match)
-;;  sagittarius : OK
+;;	guile		: OK		(guile bad-r7rs)
+;;  gosh		: FAIL      (gosh -r 7 -I . ./bad-r7rs.scm, can't find match)
+;;  sagittarius : OK		(sagittarius bad-r7rs.scm)
 ;;	chicken		: OK		(csi  -R r7rs bad-r7rs.scm)
-;;	foment		: FAIL		(foment bad-r7rs.scm)
+;;	foment		: FAIL		(foment bad-r7rs.scm syntax-violation variable "variable: bound to syntax" irritants: (...))
 ;;  gambit      : FAIL		(ggsi bad-r7rs.scm, Unbound variable: scheme)
 ;; ======================================================================
 
@@ -15,10 +15,7 @@
   (guile 
 	(use-modules (ice-9 match)))
   (gosh
-	(import (scheme base) (scheme write) (scheme read) (scheme file) 
-			;; (utils match)
-			(util match)
-			))
+	(import (scheme base) (scheme write) (scheme read) (scheme file) (util match)))
   (sagittarius
 	(import (scheme base) (scheme write) (scheme read) (scheme file) (match)))
   (chicken
@@ -61,7 +58,48 @@
 			  ((('export symbols) rest ...)
 			   (display "  exports <") (display symbols) (display ">\n")
 			   (lint-r7rs rest))
+			  ((('import symbols ...) rest ...)
+			   (display "  imports <") (display symbols) (display ">\n")
+			   (lint-r7rs rest))
+			  ((('cond-expand conditionnals ...) rest ...)
+			   (letrec ((loop (lambda(c)
+								(match c
+									   ('()
+										(lint-r7rs rest))
+									   ((('chicken more ...) others ...)
+										(cond-expand
+										  (chicken 
+											(lint-r7rs more)
+											(lint-r7rs rest))
+										  (else (loop others))
+										  ))
+									   ((('guile more ...) others ...)
+										(cond-expand
+										  (guile 
+											(lint-r7rs more)
+											(lint-r7rs rest))
+										  (else (loop others))
+										  ))
+									   ((('sagittarius more ...) others ...)
+										(cond-expand
+										  (sagittarius 
+											(lint-r7rs more)
+											(lint-r7rs rest))
+										  (else (loop others))
+										  ))
+									   (('else more ...)
+										(lint-r7rs more)
+										(lint-r7rs rest))
+									   (((compiler-name _ ...) others ...)
+										(loop others))
+									   (else 
+										 (loop (cdr c)))))))
+				 (loop conditionnals)))
+			  ((('define body ...) rest ...)
+			   (display "  define <") (display (car body)) (display ">\n")
+			   (lint-r7rs (list (cdr body) rest)))
 			  ((head rest ...)
+			   (lint-r7rs head)
 			   (lint-r7rs rest))))
 	  (else #t))))
 
@@ -71,3 +109,13 @@
 			  (display "-->")
 			  (lint-r7rs e)
 			  (display "<--\n")) p))
+
+
+#|
+((owl-lisp (import (owl defmac) (owl io) (scheme base) (scheme write) (slprintf slprintf) (slprintf format format-int) (slprintf values) (slprintf format format-int) (tools exception) (bbmatch bbmatch) (helpers) (fileOperations binFileReader)))
+ (gosh (import (scheme base) (scheme write) (util match) (slprintf slprintf) (slprintf format format-int) (slprintf values) (slprintf format format-int) (tools exception) (helpers) (fileOperations binFileReader)))
+ (sagittarius (import (scheme base) (scheme write) (match) (slprintf slprintf) (slprintf format format-int) (slprintf values) (slprintf format format-int) (tools exception) (helpers) (fileOperations binFileReader)))
+ (chicken (import (scheme base) (scheme write) (matchable) (slprintf slprintf) (slprintf format format-int) (slprintf values) (slprintf format format-int) (tools exception) (helpers) (fileOperations binFileReader)))
+ (else (import (scheme base) (scheme write) (slprintf slprintf) (slprintf format format-int) (slprintf values) (slprintf format format-int) (tools exception) (bbmatch bbmatch) (helpers) (fileOperations binFileReader)))
+ )
+|#
